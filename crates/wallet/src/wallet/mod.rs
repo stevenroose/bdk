785a2377688cfa4fd94c47d640c05befe89086f7
Steven Roose <steven@stevenroose.org>
2025-02-25 13:00:52 +0000

    wallet: Add ConfirmationSpendPolicy to spend only [un]confirmed outputs

--- crates/wallet/src/wallet/mod.rs
+++ crates/wallet/src/wallet/mod.rs
@@ -2013,6 +2013,7 @@ impl Wallet {
     ) -> (Vec<WeightedUtxo>, Vec<WeightedUtxo>) {
         let TxParams {
             change_policy,
+            confirmation_policy,
             unspendable,
             utxos,
             drain_wallet,
@@ -2082,6 +2083,7 @@ impl Wallet {
         let mut i = 0;
         may_spend.retain(|u| {
             let retain = (self.keychains().count() == 1 || change_policy.is_satisfied_by(&u.0))
+                && confirmation_policy.is_satisfied_by(&u.0)
                 && !unspendable.contains(&u.0.outpoint)
                 && satisfies_confirmed[i];
             i += 1;
